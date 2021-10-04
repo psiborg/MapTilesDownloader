0ad3529576e55470d1ccf53f536c70fa81f5ec1f
Jim Ing <jim.ing@gmail.com>
2021-10-03 23:22:58 -0400

    Log requests

--- src/utils.py
+++ src/utils.py
@@ -23,152 +23,166 @@ import math
 
 from PIL import Image
 
+import logging
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(
+    handlers=[
+        RotatingFileHandler("requests.log", maxBytes=100000000, backupCount=50)
+    ],  # 100 MB * 50 = 5 GB
+    format="[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s",
+    level=logging.DEBUG,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
 class Utils:
-	
-	@staticmethod
-	def randomString():
-		return uuid.uuid4().hex.upper()[0:6]
+    @staticmethod
+    def randomString():
+        return uuid.uuid4().hex.upper()[0:6]
 
-	def getChildTiles(x, y, z):
-		childX = x * 2
-		childY = y * 2
-		childZ = z + 1
+    def getChildTiles(x, y, z):
+        childX = x * 2
+        childY = y * 2
+        childZ = z + 1
 
-		return [
-			(childX, childY, childZ),
-			(childX+1, childY, childZ),
-			(childX+1, childY+1, childZ),
-			(childX, childY+1, childZ),
-		]
+        return [
+            (childX, childY, childZ),
+            (childX + 1, childY, childZ),
+            (childX + 1, childY + 1, childZ),
+            (childX, childY + 1, childZ),
+        ]
 
-	def makeQuadKey(tile_x, tile_y, level):
-		quadkey = ""
-		for i in range(level):
-			bit = level - i
-			digit = ord('0')
-			mask = 1 << (bit - 1)  # if (bit - 1) > 0 else 1 >> (bit - 1)
-			if (tile_x & mask) != 0:
-				digit += 1
-			if (tile_y & mask) != 0:
-				digit += 2
-			quadkey += chr(digit)
-		return quadkey
+    def makeQuadKey(tile_x, tile_y, level):
+        quadkey = ""
+        for i in range(level):
+            bit = level - i
+            digit = ord("0")
+            mask = 1 << (bit - 1)  # if (bit - 1) > 0 else 1 >> (bit - 1)
+            if (tile_x & mask) != 0:
+                digit += 1
+            if (tile_y & mask) != 0:
+                digit += 2
+            quadkey += chr(digit)
+        return quadkey
 
-	@staticmethod
-	def num2deg(xtile, ytile, zoom):
-		n = 2.0 ** zoom
-		lon_deg = xtile / n * 360.0 - 180.0
-		lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
-		lat_deg = math.degrees(lat_rad)
-		return (lat_deg, lon_deg)
+    @staticmethod
+    def num2deg(xtile, ytile, zoom):
+        n = 2.0 ** zoom
+        lon_deg = xtile / n * 360.0 - 180.0
+        lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+        lat_deg = math.degrees(lat_rad)
+        return (lat_deg, lon_deg)
 
-	@staticmethod
-	def qualifyURL(url, x, y, z):
+    @staticmethod
+    def qualifyURL(url, x, y, z):
 
-		scale22 = 23 - (z * 2)
+        scale22 = 23 - (z * 2)
 
-		replaceMap = {
-			"x": str(x),
-			"y": str(y),
-			"z": str(z),
-			"scale:22": str(scale22),
-			"quad": Utils.makeQuadKey(x, y, z),
-		}
+        replaceMap = {
+            "x": str(x),
+            "y": str(y),
+            "z": str(z),
+            "scale:22": str(scale22),
+            "quad": Utils.makeQuadKey(x, y, z),
+        }
 
-		for key, value in replaceMap.items():
-			newKey = str("{" + str(key) + "}")
-			url = url.replace(newKey, value)
+        for key, value in replaceMap.items():
+            newKey = str("{" + str(key) + "}")
+            url = url.replace(newKey, value)
 
-		return url
+        return url
 
-	@staticmethod
-	def mergeQuadTile(quadTiles):
+    @staticmethod
+    def mergeQuadTile(quadTiles):
 
-		width = 0
-		height = 0
+        width = 0
+        height = 0
 
-		for tile in quadTiles:
-			if(tile is not None):
-				width = quadTiles[0].size[0] * 2
-				height = quadTiles[1].size[1] * 2
-				break
+        for tile in quadTiles:
+            if tile is not None:
+                width = quadTiles[0].size[0] * 2
+                height = quadTiles[1].size[1] * 2
+                break
 
-		if width == 0 or height == 0:
-			return None
+        if width == 0 or height == 0:
+            return None
 
-		canvas = Image.new('RGB', (width, height))
+        canvas = Image.new("RGB", (width, height))
 
-		if quadTiles[0] is not None:
-			canvas.paste(quadTiles[0], box=(0,0))
+        if quadTiles[0] is not None:
+            canvas.paste(quadTiles[0], box=(0, 0))
 
-		if quadTiles[1] is not None:
-			canvas.paste(quadTiles[1], box=(width - quadTiles[1].size[0], 0))
+        if quadTiles[1] is not None:
+            canvas.paste(quadTiles[1], box=(width - quadTiles[1].size[0], 0))
 
-		if quadTiles[2] is not None:
-			canvas.paste(quadTiles[2], box=(width - quadTiles[2].size[0], height - quadTiles[2].size[1]))
+        if quadTiles[2] is not None:
+            canvas.paste(
+                quadTiles[2],
+                box=(width - quadTiles[2].size[0], height - quadTiles[2].size[1]),
+            )
 
-		if quadTiles[3] is not None:
-			canvas.paste(quadTiles[3], box=(0, height - quadTiles[3].size[1]))
+        if quadTiles[3] is not None:
+            canvas.paste(quadTiles[3], box=(0, height - quadTiles[3].size[1]))
 
-		return canvas
+        return canvas
 
-	@staticmethod
-	def downloadFile(url, destination, x, y, z):
+    @staticmethod
+    def downloadFile(url, destination, x, y, z):
 
-		url = Utils.qualifyURL(url, x, y, z)
+        url = Utils.qualifyURL(url, x, y, z)
 
-		code = 0
+        code = 0
 
-		# monkey patching SSL certificate issue
-		# DONT use it in a prod/sensitive environment
-		ssl._create_default_https_context = ssl._create_unverified_context
+        # monkey patching SSL certificate issue
+        # DONT use it in a prod/sensitive environment
+        ssl._create_default_https_context = ssl._create_unverified_context
 
-		try:
-			path, response = urllib.request.urlretrieve(url, destination)
-			code = 200
-		except urllib.error.URLError as e:
-			if not hasattr(e, "code"):
-				print(e)
-				code = -1
-			else:
-				code = e.code
+        try:
+            path, response = urllib.request.urlretrieve(url, destination)
+            code = 200
+        except urllib.error.URLError as e:
+            if not hasattr(e, "code"):
+                print(e)
+                code = -1
+            else:
+                code = e.code
 
-		return code
+        if code == 200:
+            logging.info(url)
+        else:
+            logging.error(url + " [" + code + "]")
 
+        return code
 
-	@staticmethod
-	def downloadFileScaled(url, destination, x, y, z, outputScale):
+    @staticmethod
+    def downloadFileScaled(url, destination, x, y, z, outputScale):
 
-		if outputScale == 1:
-			return Utils.downloadFile(url, destination, x, y, z)
+        if outputScale == 1:
+            return Utils.downloadFile(url, destination, x, y, z)
 
-		elif outputScale == 2:
+        elif outputScale == 2:
 
-			childTiles = Utils.getChildTiles(x, y, z)
-			childImages = []
+            childTiles = Utils.getChildTiles(x, y, z)
+            childImages = []
 
-			for childX, childY, childZ in childTiles:
-				
-				tempFile = Utils.randomString() + ".png"
-				tempFilePath = os.path.join("temp", tempFile)
+            for childX, childY, childZ in childTiles:
 
-				code = Utils.downloadFile(url, tempFilePath, childX, childY, childZ)
+                tempFile = Utils.randomString() + ".png"
+                tempFilePath = os.path.join("temp", tempFile)
 
-				if code == 200:
-					image = Image.open(tempFilePath)
-				else:
-					return code
+                code = Utils.downloadFile(url, tempFilePath, childX, childY, childZ)
 
-				childImages.append(image)
-			
-			canvas = Utils.mergeQuadTile(childImages)
-			canvas.save(destination, "PNG")
-			
-			return 200
+                if code == 200:
+                    image = Image.open(tempFilePath)
+                else:
+                    return code
 
-		#TODO implement custom scale
+                childImages.append(image)
 
-			
+            canvas = Utils.mergeQuadTile(childImages)
+            canvas.save(destination, "PNG")
 
+            return 200
 
-
+        # TODO implement custom scale
